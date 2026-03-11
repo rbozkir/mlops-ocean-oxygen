a6dc9f9b6da2d9beb2dd4e04d6b105af8e8525dc
rbozkir <sr.bozkir@gmail.com>
2026-03-11 15:51:43 +0300

    update preprocessing method

--- mlops_ocean_oxygen/src/model/__main__.py
+++ mlops_ocean_oxygen/src/model/__main__.py
@@ -1,5 +1,6 @@
 import ensemble_bagged_trees, gradient_boosting, knn, linear_regression, neural_network, svm
 import pandas as pd 
+from sklearn.model_selection import train_test_split
 
 water_data = pd.read_csv("data/water.csv")
 water_data = water_data[(water_data["O2ml_L"] >= 0) & (water_data["NO3uM"] >= 0)].copy()
@@ -10,9 +11,13 @@ for col in features:
     if water_data[col].isnull().any() or water_data[col].dtype == "int64":
         water_data[col] = water_data[col].astype("float64")
 
-ensemble_bagged_trees.run_experiment(water_data, features, target)
-gradient_boosting.run_experiment(water_data, features, target)
-knn.run_experiment(water_data, features, target)
-linear_regression.run_experiment(water_data, features, target)
-neural_network.run_experiment(water_data, features, target)
-svm.run_experiment(water_data, features, target)
+Xtrain, Xtest, ytrain, ytest = train_test_split(water_data[features], water_data[target], train_size=80, random_state=42)
+eval_data = Xtest.copy()
+eval_data["target"] = ytest
+
+ensemble_bagged_trees.run_experiment(Xtrain, ytrain, eval_data)
+gradient_boosting.run_experiment(Xtrain, ytrain, eval_data)
+knn.run_experiment(Xtrain, ytrain, eval_data)
+linear_regression.run_experiment(Xtrain, ytrain, eval_data)
+neural_network.run_experiment(Xtrain, ytrain, eval_data)
+svm.run_experiment(Xtrain, ytrain, eval_data)
